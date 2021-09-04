bdace99a4abe23a6ac700b33ceda8736d1c505bf
clam65 <78088949+clam65@users.noreply.github.com>
2021-09-04 19:52:17 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,95 @@
 
 
 
-let now = new Date();
+function getDateTime() {
+    var timeNow  = new Date(); 
+    var year    = timeNow.getFullYear();
+    var month   = timeNow.getMonth()+1; 
+    var day     = timeNow.getDate();
+    var dayOfWeek = timeNow.getDay();
+    var hour    = timeNow.getHours();
+    var minute  = timeNow.getMinutes();
+    var second  = timeNow.getSeconds(); 
+    if(month.toString().length == 1) {
+         month = '0'+month;
+    }
+    if(day.toString().length == 1) {
+         day = '0'+day;
+    }   
+    if(hour.toString().length == 1) {
+         hour = '0'+hour;
+    }
+    if(minute.toString().length == 1) {
+         minute = '0'+minute;
+    }
+    if(second.toString().length == 1) {
+         second = '0'+second;
+    }   
+    var date = month+'/'+year;  
+    var time = hour+':'+minute+':'+second; 
+    var dayOfWeekString = ["Mon", "Tues", "Wed", "Thur", "Fri", "Sat", "Sun"];
+    return [date, time, day, dayOfWeekString[dayOfWeek - 1]];
+}
 
 
-document.getElementById("Clock-Display").innerHTML = " Hello" ;
+setInterval(function(){
+    currentTime = getDateTime();
+    document.getElementById("Clock-Display").innerHTML = currentTime[1];
+    document.getElementById("Date-Display").innerHTML = currentTime[0];
+    document.getElementById("Day-Display").innerHTML = currentTime[2];
+    document.getElementById("Day-Of-Week-Display").innerHTML = currentTime[3];
+}, 1000);
 
-// now.getHours() + now.getMinutes() + now.getSeconds();
+document.getElementById("Toggle").addEventListener("click", function() {
+
+   
+    
+    let setAlarmDayOfWeek = "1";
+    let setAlarmHour = "0";
+    let setAlarmMinute = "0";
+    let setAlarm = setAlarmHour + ':' + setAlarmMinute;
+    
+    document.getElementById("Clock-Display").innerHTML = setAlarm;
+    document.getElementById("Day-Display").innerHTML = setAlarmDayOfWeek;
+
+    document.getElementById("Toggle").addEventListener("click", function() {
+
+           
+        getDateTime();
+
+    });
+    
+
+    function setHour {
+
+        setAlarmHour += 1;
+        return setAlarmHour;
+
+    }
+
+    function setMinute {
+
+        set AlarmMinute +=1;
+        return setAlarmMinute;
+        
+    }
+
+    function setDay {
+
+
+
+
+
+    }
+
+}); 
+   
+  
+   
+    
+
+    
+
+
+
+
